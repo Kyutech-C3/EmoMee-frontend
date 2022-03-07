53cb52895276afb5b4fdc869059e52ccfe8e136e
kyuuri11013 <kyuuri11013@gmail.com>
2022-03-07 22:30:51 +0900

    add: google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,15 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          'M+PLUS+Rounded+1c': true,
+        },
+        display: 'swap',
+      },
+    ],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
