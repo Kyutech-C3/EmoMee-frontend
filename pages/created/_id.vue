66e603d615cc2f04ff1899588e6bef629ba33b99
catgolem <kugi1255@icloud.com>
2022-03-07 10:46:36 +0000

    [add] pages/created/_id

---
+++ pages/created/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="flex items-center justify-center w-screen h-screen flex-col relative"
+  >
+    <room-info class="w-192 h-80 z-10" />
+    <base-button class="m-12 px-16 py-5 text-3xl shadow-xl">作成</base-button>
+    <select-base-face-bar></select-base-face-bar>
+    <select-reaction-bar></select-reaction-bar>
+  </div>
+</template>
+
+<script>
+import RoomInfo from '../../components/RoomInfo.vue'
+import BaseButton from '../../components/BaseButton.vue'
+import SelectReactionBar from '../../components/SelectReactionBar.vue'
+import SelectBaseFaceBar from '~/components/SelectBaseFaceBar.vue'
+
+export default {
+  components: {
+    SelectReactionBar,
+    RoomInfo,
+    BaseButton,
+    SelectBaseFaceBar,
+  },
+}
+</script>
