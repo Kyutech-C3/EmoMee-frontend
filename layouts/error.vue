e19184bbcb783cffbb5f40f8f3d590e738863f1e
kyuuri11013 <kyuuri11013@gmail.com>
2022-03-10 19:20:46 +0900

    fix: エラーページの画像パス

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,7 @@
       class="w-fit border border-gray-300 rounded-xl text-center px-16 py-10 h-fit absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-3xl bg-white bg-opacity-70 shadow-inner-xl"
     >
       <img
-        src="@/assets/pigeon/sad.gif"
+        src="@/assets/face/pigeon/sad.gif"
         alt="sad pigeon"
         class="absolute -bottom-20 -right-10 w-28"
       />
